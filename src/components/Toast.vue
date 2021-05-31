fd9e12f8fc0d374941b367a31ab082a39d1f3a8f
zzkung <zzkung@outlook.com>
2021-05-31 11:44:05 +0800

    add toast.vue

---
+++ src/components/Toast.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="toast">{{message}}</div>
+</template>
+
+<script>
+export default {
+  name: 'Toast',
+  props: ['message']
+}
+
+</script>
+
+<style lang='scss' scoped>
+.toast {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: .1rem;
+  background: rgba($color: #000000, $alpha: .35);
+  border-radius: .05rem;
+  color: #fff;
+}
+</style>
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -9,6 +9,7 @@
     </div>
     <div class="wrapper__login-button" @click="handleLogin">登录</div>
     <div class="wrapper__login-link" @click="handleRegisterClick">立即注册</div>
+    <Toast v-if="data.showToast" :message="data.toastMessage" />
   </div>
 </template>
 
@@ -16,19 +17,34 @@
 import { useRouter } from 'vue-router'
 import { post } from '../../utils/request'
 import { reactive } from 'vue'
+import Toast from '../../components/Toast'
 
 export default {
   name: 'Login',
 
+  components: { Toast },
+
   setup () {
     const data = reactive({
       username: '',
-      password: ''
+      password: '',
+      showToast: false,
+      toastMessage: ''
     })
     const router = useRouter()
+
+    const showToast = (message) => {
+      data.showToast = true
+      data.toastMessage = message
+      setTimeout(() => {
+        data.showToast = false
+        data.toastMessage = ''
+      }, 2000)
+    }
+
     const handleLogin = async () => {
       try {
-        const result = await post('/api/user/login', {
+        const result = await post('1/api/user/login', {
           username: data.username,
           password: data.password
         })
@@ -38,10 +54,10 @@ export default {
           localStorage.isLogin = true
           router.push({ name: 'Home' })
         } else {
-          alert('登录失败')
+          showToast('登录失败')
         }
       } catch (e) {
-        alert('请求失败')
+        showToast('请求失败')
       }
     }
 
